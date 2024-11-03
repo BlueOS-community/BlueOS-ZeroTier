76e3b1643c9b8ba60017d255f3524f408dcdbceb
Willian Galvani <williangalvani@gmail.com>
2024-11-03 18:24:25 -0300

    Update UI, expose local.conf file to user

--- web/main.py
+++ web/main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from typing import Any
 
 import appdirs
+from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
@@ -20,9 +21,12 @@ SERVICE_NAME = "zerotiermanager"
 # logging.basicConfig(handlers=[InterceptHandler()], level=0)
 #logger.add(get_new_log_path(SERVICE_NAME))
 
+class Settings(BaseModel):
+    settings: str
+
 app = FastAPI(
     title="ZeroTierManager API",
-    description="ZeroTierManager is an extension to provice ZeroTier connectivity to BlueOS",
+    description="ZeroTierManager is an extension to provide ZeroTier connectivity to BlueOS",
 )
 # app.router.route_class = GenericErrorHandlingRoute
 logger.info("Starting ZeroTierManager!")
@@ -44,7 +48,7 @@ async def command_join(network: str) -> Any:
     command = f'zerotier-cli join {network}'
     logger.debug(f"Running command: {command}")
     output = run_command(command, False)
-    return output
+    return output.stdout or output.stderr
 
 @app.post("/command/leave", status_code=status.HTTP_200_OK)
 @version(1, 0)
@@ -52,7 +56,7 @@ async def command_leave(network: str) -> Any:
     command = f'zerotier-cli leave {network}'
     logger.debug(f"Running command: {command}")
     output = run_command(command, False)
-    return output
+    return output.stdout or output.stderr
 
 @app.get("/command/info", status_code=status.HTTP_200_OK)
 @version(1, 0)
@@ -60,7 +64,7 @@ async def command_info() -> Any:
     command = f'zerotier-cli info'
     logger.debug(f"Running command: {command}")
     output = run_command(command, False)
-    return output
+    return output.stdout or output.stderr
 
 @app.get("/command/networks", status_code=status.HTTP_200_OK)
 @version(1, 0)
@@ -68,8 +72,26 @@ async def command_info() -> Any:
     command = f'zerotier-cli listnetworks -j'
     logger.debug(f"Running command: {command}")
     output = run_command(command, False)
-    return output
+    return output.stdout or output.stderr
 
+@app.get("/settings", status_code=status.HTTP_200_OK)
+@version(1, 0)
+async def read_conf() -> Any:
+    try:
+        with open('/var/lib/zerotier-one/local.conf', 'r') as f:
+            return f.read()
+    except:
+        pass
+    return "{}"
+
+@app.post("/settings", status_code=status.HTTP_200_OK)
+@version(1, 0)
+async def write_conf(conf: Settings) -> Any:
+  path = Path('/var/lib/zerotier-one/local.conf')
+  path.parent.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+  with path.open('w') as f:
+    f.write(conf.settings)
+  return conf
 
 app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
 
